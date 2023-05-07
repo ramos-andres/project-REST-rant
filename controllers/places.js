ebf08a2837ea328db4e9aaae4ccadd5a4a5f18bc
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-05-07 09:07:28 -0700

    Added method override and href values to edit and delete buttons

--- controllers/places.js
+++ controllers/places.js
@@ -30,5 +30,21 @@ router.get('/', (req, res) => {
     res.render('places/new')
   })
   
+  // GET /places/:id
+  router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      res.render('places/show', {place: places[id], id})
+    }
+  })
+  
+  
+  
 //exports
 module.exports = router
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //dependencies
 require('dotenv').config()
 const express = require('express')
+const methodOverride = require('method-override')
 
 //configuration
 const app = express()
@@ -10,6 +11,7 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 
 //places controller
