4e0bac86c490c87e5d03e449d38ca27cc0e31b18
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-05-07 14:59:24 -0700

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -9,69 +9,97 @@ router.get('/', (req, res) => {
     res.render('places/index', {places})
   })
   
-  // POST /places
-  router.post('/', (req, res) => {
-    if (!req.body.pic) {
-      // Default image if one is not provided
-      req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-      req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-      req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.redirect('POST /places')
-  })
+// POST /places
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('POST /places')
+})
   
-  // GET /places/new
-  router.get('/new', (req, res) => {
-    res.render('places/new')
-  })
+// GET /places/new
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
   
-  // GET /places/:id
-  router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
+// GET /places/:id
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', {place: places[id], id})
+  }
+})
+
+// PUT /places/:id
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
       res.render('error404')
-    }
-    else if (!places[id]) {
+  }
+  else if (!places[id]) {
       res.render('error404')
-    }
-    else {
-      res.render('places/show', {place: places[id], id})
-    }
-  })
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
+
+// delete /places/:id
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
   
-  // delete /places/:id
-  router.delete('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
+// GET /places/:id/edit
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
       res.render('error404')
-    }
-    else if (!places[id]) {
+  }
+  else if (!places[id]) {
       res.render('error404')
-    }
-    else {
-      places.splice(id, 1)
-      res.redirect('/places')
-    }
-  })
-  
-  // GET /places/:id/edit
-  router.get('/:id/edit', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-        res.render('error404')
-    }
-    else if (!places[id]) {
-        res.render('error404')
-    }
-    else {
-      res.render('places/edit', { place: places[id] })
-    }
-  })  
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
   
 //exports
 module.exports = router
