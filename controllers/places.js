527be084d84578531c18f21ef4ae860c3e00435a
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-05-06 23:59:02 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,30 +1,28 @@
 //confusing; assignment only had "const router = require('express').Router()", had to search web
 const express = require('express')
 const router = require('express').Router()
+const places = require('../models/places.js')
 const app = express()
 
 // GET /places
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'images/thairestaurant.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/catcafe.jpg'
-      }]
     res.render('places/index', {places})
   })
   
   // POST /places
   router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('POST /places')
   })
   
   // GET /places/new
---
+++ models/places.js
@@ -0,0 +1,14 @@
+//places array temporary variable content from places index route
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/thairestaurant.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/catcafe.jpg'    
+}]
