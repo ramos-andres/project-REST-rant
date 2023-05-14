883da9c9924fc0baa26c9373804c54f4b64e4850
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-05-14 09:03:21 -0700

    Added a helpher method

--- models/places.js
+++ models/places.js
@@ -1,6 +1,5 @@
 //require mongoose
 const mongoose = require('mongoose')
-const { Schema } = mongoose
 
 //create a schema for defining a place
 const placeSchema = new mongoose.Schema({
@@ -11,5 +10,9 @@ const placeSchema = new mongoose.Schema({
   state: { type: String, default: 'USA' },
   founded: Number
 })
+//add a method to the schema
+placeSchema.methods.showEstablished = function() {
+  return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
+}
 //export the schema as a model
 module.exports = mongoose.model('Place', placeSchema)
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,11 +7,24 @@ function show (data) {
     return (
         <Def>
           <main>
+          <div className="row">
+              <div className="col-sm-6">
+                <img src={data.place.pic} alt={data.place.name} />
+                <h3>
+                  Located in {data.place.city}, {data.place.state}
+                </h3>
+              </div>
+              <div className="col-sm-6">
             <h1>{ data.place.name }</h1>
             <h2>Rating</h2>
               <h3>currently unrated</h3>
             <h2>Description</h2>
-              <h3>TBD</h3>
+              <h3>
+                {data.place.showEstablished()}
+              </h3>
+              <h4>
+                Serving {data.place.cuisines}
+              </h4>
             <h2>Comments</h2>
               <h3>No comments yet!</h3>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
@@ -21,7 +34,9 @@ function show (data) {
               <button type="submit" className="btn btn-danger">
                 Delete
               </button>
-            </form>     
+            </form>  
+              </div>
+          </div>   
           </main>
         </Def>
     )
