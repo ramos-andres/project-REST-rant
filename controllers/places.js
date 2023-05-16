4b590b9877bbf1fb0c5473fc83fb1e580e5a44b7
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-05-15 21:33:23 -0700

    Updated controllers for creating a comment

--- controllers/places.js
+++ controllers/places.js
@@ -65,9 +65,36 @@ router.get('/:id/edit', (req, res) => {
   res.send('GET edit form stub')
 })
 
-router.post('/:id/rant', (req, res) => {
-  res.send('GET /places/:id/rant stub')
+//comment routes
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  if (req.body.rant) {
+    req.body.rant = true
+  } 
+  else {
+    req.body.rant = false
+  }
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
+
+
+
 
 router.delete('/:id/rant/:rantId', (req, res) => {
     res.send('GET /places/:id/rant/:rantId stub')
