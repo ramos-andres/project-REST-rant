914b5f3c1f7c2524ce73972490fb01e88c4350e1
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-05-15 20:44:59 -0700

    Added comment to places

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -4,6 +4,25 @@ const Def = require('../default')
 
 //show stub content
 function show (data) {
+    let comments = (
+      <h3 className="inactive">
+        No comments yet!
+      </h3>
+    )
+    if (data.place.comments.length) {
+      comments = data.place.comments.map(c => {
+        return (
+          <div className="border">
+            <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
+            <h4>{c.content}</h4>
+            <h3>
+              <stong>- {c.author}</stong>
+            </h3>
+            <h4>Rating: {c.stars}</h4>
+          </div>
+        )
+      })
+    }
     return (
         <Def>
           <main>
@@ -26,7 +45,7 @@ function show (data) {
                 Serving {data.place.cuisines}
               </h4>
             <h2>Comments</h2>
-              <h3>No comments yet!</h3>
+              {comments}
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
               Edit
             </a>
