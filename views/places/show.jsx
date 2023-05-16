ca1b7906136af8d9107a4c1390c5c055790ae048
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-05-15 22:09:03 -0700

    Added stars display for ratings

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -18,12 +18,16 @@ function show (data) {
       let sumRatings = data.place.comments.reduce((tot, c) => {
         return tot + c.stars
       }, 0)
-      let averageRating = sumRatings / data.place.comments.length
+      let averageRating = Math.round(sumRatings / data.place.comments.length)
+      let stars = ''
+      for (let i = 0; i < averageRating; i++) {
+        stars += '⭐'
+      }
       rating = (
         <h3>
-          {averageRating} stars
+          {stars} stars
         </h3>
-      )
+      )    
       comments = data.place.comments.map(c => {
         return (
           <div className="border">
