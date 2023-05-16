57c103df470d72abf48b591ecf673de2bca23046
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-05-15 22:41:02 -0700

    Updated PUT and DELETE routes, updated edit form

--- controllers/places.js
+++ controllers/places.js
@@ -52,18 +52,41 @@ router.get('/:id', (req, res) => {
   })
 })
 
-
+//route to update a place
 router.put('/:id', (req, res) => {
-  res.send('PUT /places/:id stub')
+  db.Place.findByIdAndUpdate(req.params.id, req.body)
+  .then(() => {
+      res.redirect(`/places/${req.params.id}`)
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
 })
 
+
+//route to delete a place
 router.delete('/:id', (req, res) => {
-  res.send('DELETE /places/:id stub')
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/edit', { place })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
 
+//route to edit a place
 router.get('/:id/edit', (req, res) => {
-  res.send('GET edit form stub')
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/edit', { place })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
 })
+
 
 //comment routes
 router.post('/:id/comment', (req, res) => {
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -63,7 +63,7 @@ function show (data) {
               <h4>
                 Serving {data.place.cuisines}
               </h4>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+            <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> 
               Edit
             </a>
             <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
